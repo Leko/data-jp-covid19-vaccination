fc3516f60bf6954b924cac07fb81abe8c9a7203b
Leko <leko.noor@gmail.com>
2021-05-13 18:24:54 +0800

    Fix type errors

--- src/formatters/csv.ts
+++ src/formatters/csv.ts
@@ -9,7 +9,7 @@ export async function exportAsCSV(
 ): Promise<string> {
   const filePath = path.join(dir, `${baseName}.csv`)
   return new Promise((resolve, reject) => {
-    writeToPath(filePath, [headers].concat(data))
+    writeToPath(filePath, [headers].concat(data.map((row) => row.map(String))))
       .on('error', reject)
       .on('finish', resolve)
   }).then(() => filePath)
